7c9084da5d3bde979db1d995425b88da64b03643
Romain Beauxis <toots@rastageeks.org>
2025-01-16 17:24:37 -0600

    Fix getopt C type.

--- posix-getopt/src/stubs/posix_getopt_stubs.ml
+++ posix-getopt/src/stubs/posix_getopt_stubs.ml
@@ -5,12 +5,14 @@ module Def (F : Cstubs.FOREIGN) = struct
   module Types = Posix_getopt_types.Def (Posix_getopt_generated_types)
   include Types
 
-  let getopt = foreign "getopt" (int @-> ptr string @-> string @-> returning int)
+  let const_string_ptr = typedef (ptr string) "char * const *"
+
+  let getopt = foreign "getopt" (int @-> const_string_ptr @-> string @-> returning int)
   let has_getopt_long = foreign "has_getopt_long" (void @-> returning bool)
 
   let getopt_long =
     foreign "getopt_long"
-      (int @-> ptr string @-> string @-> ptr Option.t @-> ptr int
+      (int @-> const_string_ptr @-> string @-> ptr Option.t @-> ptr int
      @-> returning int)
 
   let has_getopt_long_only =
