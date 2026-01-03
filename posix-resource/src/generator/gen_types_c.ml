6ef9c7f7739730f70542f7af8485b1a43dbd67e4
Romain Beauxis <romain.beauxis@gmail.com>
2026-01-03 10:06:59 -0600

    Regenerate, add lower bound for integers.

--- posix-resource/src/generator/gen_types_c.ml
+++ posix-resource/src/generator/gen_types_c.ml
@@ -1,7 +1,8 @@
 module Types = Posix_base.Generators.Types (struct
   module Types = Posix_resource_types.Def
 
-  let c_headers = {|
+  let c_headers =
+    {|
 #include <sys/resource.h>
 #include <sys/time.h>
 #include <time.h>
