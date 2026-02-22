cfd9bfb0fcedf7f1d3e6bcb5fbdde2250353ffcf
Romain Beauxis <romain.beauxis@gmail.com>
2026-02-21 19:42:05 -0600

    More doc.

--- modules/stat/src/posix_stat.mli
+++ modules/stat/src/posix_stat.mli
@@ -1,196 +1,261 @@
-(** High-level API to <sys/stat.h>. See:
-    {{:https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_stat.h.html}
-     sys/stat.h} for details on data structures and functions. *)
+(** POSIX file status and permissions.
 
-(** {2 File mode constants} *)
+    This module provides OCaml bindings to POSIX file status functions defined in
+    {{:https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_stat.h.html} sys/stat.h}.
 
-(** {3 File type constants} *)
+    It includes functions for querying file metadata, changing permissions,
+    and creating directories and special files.
 
-(** File type mask *)
+    {2 Example}
+
+    {[
+      (* Get file information *)
+      let info = stat "/etc/passwd" in
+      Printf.printf "Size: %Ld bytes\n" (Posix_types.Off.to_int64 info.st_size);
+
+      (* Check if it's a regular file *)
+      if s_isreg info.st_mode then
+        print_endline "Regular file"
+    ]} *)
+
+(** {1 File Type Constants}
+
+    Constants for the file type portion of [st_mode].
+    Use {!s_ifmt} to mask out the file type bits. *)
+
+(** File type mask. Use as [Posix_types.Mode.logand mode s_ifmt]. *)
 val s_ifmt : Posix_types.mode_t
 
-(** Regular file *)
+(** Regular file (S_IFREG). *)
 val s_ifreg : Posix_types.mode_t
 
-(** Directory *)
+(** Directory (S_IFDIR). *)
 val s_ifdir : Posix_types.mode_t
 
-(** Symbolic link *)
+(** Symbolic link (S_IFLNK). *)
 val s_iflnk : Posix_types.mode_t
 
-(** Character device *)
+(** Character device (S_IFCHR). *)
 val s_ifchr : Posix_types.mode_t
 
-(** Block device *)
+(** Block device (S_IFBLK). *)
 val s_ifblk : Posix_types.mode_t
 
-(** FIFO/pipe *)
+(** FIFO/named pipe (S_IFIFO). *)
 val s_ififo : Posix_types.mode_t
 
-(** Socket *)
+(** Socket (S_IFSOCK). *)
 val s_ifsock : Posix_types.mode_t
 
-(** {3 Permission bit constants} *)
+(** {1 Permission Bit Constants}
 
-(** Set-user-ID on execution *)
+    Constants for file permission bits in [st_mode]. *)
+
+(** {2 Special Mode Bits} *)
+
+(** Set-user-ID on execution (S_ISUID). *)
 val s_isuid : Posix_types.mode_t
 
-(** Set-group-ID on execution *)
+(** Set-group-ID on execution (S_ISGID). *)
 val s_isgid : Posix_types.mode_t
 
-(** Sticky bit *)
+(** Sticky bit (S_ISVTX). On directories, restricts file deletion. *)
 val s_isvtx : Posix_types.mode_t
 
-(** Owner: read, write, execute *)
+(** {2 Owner Permissions} *)
+
+(** Owner: read, write, execute (S_IRWXU = 0o700). *)
 val s_irwxu : Posix_types.mode_t
 
-(** Owner: read permission *)
+(** Owner: read permission (S_IRUSR = 0o400). *)
 val s_irusr : Posix_types.mode_t
 
-(** Owner: write permission *)
+(** Owner: write permission (S_IWUSR = 0o200). *)
 val s_iwusr : Posix_types.mode_t
 
-(** Owner: execute permission *)
+(** Owner: execute permission (S_IXUSR = 0o100). *)
 val s_ixusr : Posix_types.mode_t
 
-(** Group: read, write, execute *)
+(** {2 Group Permissions} *)
+
+(** Group: read, write, execute (S_IRWXG = 0o070). *)
 val s_irwxg : Posix_types.mode_t
 
-(** Group: read permission *)
+(** Group: read permission (S_IRGRP = 0o040). *)
 val s_irgrp : Posix_types.mode_t
 
-(** Group: write permission *)
+(** Group: write permission (S_IWGRP = 0o020). *)
 val s_iwgrp : Posix_types.mode_t
 
-(** Group: execute permission *)
+(** Group: execute permission (S_IXGRP = 0o010). *)
 val s_ixgrp : Posix_types.mode_t
 
-(** Others: read, write, execute *)
+(** {2 Others Permissions} *)
+
+(** Others: read, write, execute (S_IRWXO = 0o007). *)
 val s_irwxo : Posix_types.mode_t
 
-(** Others: read permission *)
+(** Others: read permission (S_IROTH = 0o004). *)
 val s_iroth : Posix_types.mode_t
 
-(** Others: write permission *)
+(** Others: write permission (S_IWOTH = 0o002). *)
 val s_iwoth : Posix_types.mode_t
 
-(** Others: execute permission *)
+(** Others: execute permission (S_IXOTH = 0o001). *)
 val s_ixoth : Posix_types.mode_t
 
-(** {2 File type test functions} *)
+(** {1 File Type Test Functions}
 
-(** File type test functions (equivalent to POSIX S_IS* macros). These test the
-    st_mode field to determine file type. *)
+    These functions test the [st_mode] field to determine file type.
+    They are equivalent to the POSIX S_IS* macros. *)
 
-(** Test for regular file *)
+(** [s_isreg mode] returns [true] if [mode] indicates a regular file. *)
 val s_isreg : Posix_types.mode_t -> bool
 
-(** Test for directory *)
+(** [s_isdir mode] returns [true] if [mode] indicates a directory. *)
 val s_isdir : Posix_types.mode_t -> bool
 
-(** Test for symbolic link *)
+(** [s_islnk mode] returns [true] if [mode] indicates a symbolic link. *)
 val s_islnk : Posix_types.mode_t -> bool
 
-(** Test for character device *)
+(** [s_ischr mode] returns [true] if [mode] indicates a character device. *)
 val s_ischr : Posix_types.mode_t -> bool
 
-(** Test for block device *)
+(** [s_isblk mode] returns [true] if [mode] indicates a block device. *)
 val s_isblk : Posix_types.mode_t -> bool
 
-(** Test for FIFO/pipe *)
+(** [s_isfifo mode] returns [true] if [mode] indicates a FIFO/named pipe. *)
 val s_isfifo : Posix_types.mode_t -> bool
 
-(** Test for socket *)
+(** [s_issock mode] returns [true] if [mode] indicates a socket. *)
 val s_issock : Posix_types.mode_t -> bool
 
-(** {2 File status structure} *)
+(** {1 File Status Structure} *)
 
-(** High-level representation of struct stat. All fields are accessible as
-    record fields. Timestamps use {!Posix_time2.Timespec.t}. *)
+(** File status information returned by {!stat}, {!fstat}, and {!lstat}.
+    Corresponds to POSIX [struct stat]. *)
 type stat = {
-  st_dev : Posix_types.dev_t;  (** Device ID of device containing file *)
-  st_ino : Posix_types.ino_t;  (** File serial number *)
-  st_mode : Posix_types.mode_t;  (** File mode (type and permissions) *)
-  st_nlink : Posix_types.nlink_t;  (** Number of hard links *)
-  st_uid : Posix_types.uid_t;  (** User ID of file owner *)
-  st_gid : Posix_types.gid_t;  (** Group ID of file owner *)
-  st_rdev : Posix_types.dev_t;  (** Device ID (if special file) *)
-  st_size : Posix_types.off_t;  (** File size in bytes *)
-  st_atim : Posix_time2.Timespec.t;  (** Time of last access *)
-  st_mtim : Posix_time2.Timespec.t;  (** Time of last modification *)
-  st_ctim : Posix_time2.Timespec.t;  (** Time of last status change *)
-  st_blksize : Posix_types.blksize_t;  (** Optimal block size for I/O *)
-  st_blocks : Posix_types.blkcnt_t;  (** Number of 512-byte blocks allocated *)
+  st_dev : Posix_types.dev_t;
+  st_ino : Posix_types.ino_t;
+  st_mode : Posix_types.mode_t;
+  st_nlink : Posix_types.nlink_t;
+  st_uid : Posix_types.uid_t;
+  st_gid : Posix_types.gid_t;
+  st_rdev : Posix_types.dev_t;
+  st_size : Posix_types.off_t;
+  st_atim : Posix_time2.Timespec.t;
+  st_mtim : Posix_time2.Timespec.t;
+  st_ctim : Posix_time2.Timespec.t;
+  st_blksize : Posix_types.blksize_t;
+  st_blocks : Posix_types.blkcnt_t;
 }
 
-(** {2 File status functions} *)
+(** {1 File Status Functions} *)
 
-(** [stat path] returns file status for the file at [path]. Follows symbolic
-    links.
-    @raise Unix.Unix_error on failure *)
+(** Get file status, following symbolic links.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/stat.html} stat(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val stat : string -> stat
 
-(** [fstat fd] returns file status for the open file descriptor [fd].
-    @raise Unix.Unix_error on failure *)
+(** Get file status for an open file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/fstat.html} fstat(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val fstat : Unix.file_descr -> stat
 
-(** [lstat path] returns file status for the file at [path]. Does not follow
-    symbolic links (returns info about the link itself).
-    @raise Unix.Unix_error on failure *)
+(** Get file status without following symbolic links.
+
+    Returns information about the symbolic link itself, not its target.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/lstat.html} lstat(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val lstat : string -> stat
 
-(** {2 Permission change functions} *)
+(** {1 Permission Functions} *)
 
-(** [chmod path mode] changes the file mode (permissions) of [path].
-    @raise Unix.Unix_error on failure *)
+(** Change file permissions.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/chmod.html} chmod(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val chmod : string -> Posix_types.mode_t -> unit
 
-(** [fchmod fd mode] changes the file mode of the open file descriptor [fd].
-    @raise Unix.Unix_error on failure *)
+(** Change file permissions using a file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/fchmod.html} fchmod(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val fchmod : Unix.file_descr -> Posix_types.mode_t -> unit
 
-(** {2 Directory and special file creation} *)
+(** {1 File Creation} *)
 
-(** [mkdir path mode] creates a new directory at [path] with permissions [mode].
-    @raise Unix.Unix_error on failure *)
+(** Create a directory.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/mkdir.html} mkdir(2)}.
+
+    @raise Unix.Unix_error on failure. *)
 val mkdir : string -> Posix_types.mode_t -> unit
 
-(** [mkfifo path mode] creates a FIFO special file at [path] with permissions
-    [mode].
-    @raise Unix.Unix_error on failure *)
+(** Create a FIFO (named pipe).
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/mkfifo.html} mkfifo(3)}.
+
+    @raise Unix.Unix_error on failure. *)
 val mkfifo : string -> Posix_types.mode_t -> unit
 
-(** {2 File creation mask} *)
+(** {1 File Creation Mask} *)
 
-(** [umask mask] sets the file mode creation mask and returns the previous
-    value. The mask determines which permission bits are turned off when
-    creating new files. *)
+(** Set the file mode creation mask.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/umask.html} umask(2)}.
+
+    The umask is used to turn off permission bits when creating files.
+    For example, [umask (Mode.of_int 0o022)] prevents group and others
+    write permissions on newly created files.
+
+    @return The previous umask value. *)
 val umask : Posix_types.mode_t -> Posix_types.mode_t
 
-(** {2 Advanced *at functions} *)
+(** {1 Directory-Relative Operations}
 
-(** Constants for *at functions *)
+    These functions operate relative to a directory file descriptor,
+    providing protection against certain race conditions (TOCTOU).
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/fstatat.html} *at(2)} functions. *)
+
+(** Use current working directory as the base for relative paths.
+    Pass this as [dirfd] to use CWD like the non-[*at] functions. *)
 val at_fdcwd : int
-(** Use current working directory *)
 
-(** Don't follow symbolic links *)
+(** Flag: do not follow symbolic links. *)
 val at_symlink_nofollow : int
 
-(** Remove directory instead of file *)
+(** Flag: remove directory instead of file (for unlinkat). *)
 val at_removedir : int
 
-(** Use effective IDs for access check *)
+(** Flag: use effective IDs for access checks (for faccessat). *)
 val at_eaccess : int
 
-(** [fstatat ?dirfd ?flags path] is like {!stat} but interprets [path] relative
-    to directory [dirfd]. If [dirfd] is omitted, uses current working directory.
-    [flags] may include {!at_symlink_nofollow}.
-    @raise Unix.Unix_error on failure *)
+(** Like {!stat}/{!lstat} but relative to a directory file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/fstatat.html} fstatat(2)}.
+
+    @param dirfd Base directory (default: current working directory).
+    @param flags May include {!at_symlink_nofollow} to not follow symlinks.
+    @raise Unix.Unix_error on failure. *)
 val fstatat : ?dirfd:Unix.file_descr -> ?flags:int list -> string -> stat
 
-(** [fchmodat ?dirfd ?flags path mode] is like {!chmod} but interprets [path]
-    relative to directory [dirfd].
-    @raise Unix.Unix_error on failure *)
+(** Like {!chmod} but relative to a directory file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/fchmodat.html} fchmodat(2)}.
+
+    @param dirfd Base directory (default: current working directory).
+    @param flags Optional flags.
+    @raise Unix.Unix_error on failure. *)
 val fchmodat :
   ?dirfd:Unix.file_descr ->
   ?flags:int list ->
@@ -198,12 +263,18 @@ val fchmodat :
   Posix_types.mode_t ->
   unit
 
-(** [mkdirat ?dirfd path mode] is like {!mkdir} but interprets [path] relative
-    to directory [dirfd].
-    @raise Unix.Unix_error on failure *)
+(** Like {!mkdir} but relative to a directory file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/mkdirat.html} mkdirat(2)}.
+
+    @param dirfd Base directory (default: current working directory).
+    @raise Unix.Unix_error on failure. *)
 val mkdirat : ?dirfd:Unix.file_descr -> string -> Posix_types.mode_t -> unit
 
-(** [mkfifoat ?dirfd path mode] is like {!mkfifo} but interprets [path] relative
-    to directory [dirfd].
-    @raise Unix.Unix_error on failure *)
+(** Like {!mkfifo} but relative to a directory file descriptor.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/mkfifoat.html} mkfifoat(3)}.
+
+    @param dirfd Base directory (default: current working directory).
+    @raise Unix.Unix_error on failure. *)
 val mkfifoat : ?dirfd:Unix.file_descr -> string -> Posix_types.mode_t -> unit
--- modules/time2/src/posix_time2.mli
+++ modules/time2/src/posix_time2.mli
@@ -1,95 +1,98 @@
-(** High-level API to <time.h> and <sys/time.h>. See:
-    {{:https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/time.h.html}
-     time.h} and
-    {{:https://pubs.opengroup.org/onlinepubs/7908799/xsh/systime.h.html}
-     systime.h} for an explanation about the data structures and functions. *)
+(** POSIX time and clock functions.
 
+    This module provides OCaml bindings to POSIX time functions defined in
+    {{:https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/time.h.html} time.h}
+    and {{:https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/sys_time.h.html} sys/time.h}.
+
+    It includes high-precision time structures, clock access, timers, and
+    time conversion functions. *)
+
+(** {1 Time Structures} *)
+
+(** POSIX timespec: time with nanosecond precision. *)
 module Timespec : sig
-  (** POSIX timespec time specifier with seconds and nanoseconds. A normalized
-      value of type [t] must have a value of [tv_nsec] between [0] and
-      [1000000000] (exclusive). *)
+  (** Time value with seconds and nanoseconds. A normalized value has [tv_nsec]
+      between [0] and [999999999] (inclusive). *)
   type t = private { tv_sec : int64; tv_nsec : int64 }
 
-  (** [create sec nsec] creates a new normalized timespec with [sec] seconds and
-      [nsec] nanoseconds. *)
+  (** [create sec nsec] creates a new normalized timespec. *)
   val create : int64 -> int64 -> t
 
-  (** [add t1 t2] adds the two timespec values, returning a normalized value *)
+  (** [add t1 t2] adds two timespec values, returning a normalized result. *)
   val add : t -> t -> t
 
-  (** [sub t1 t2] subtracts [t2] from [t1], creating a new normalized time
-      value. *)
+  (** [sub t1 t2] subtracts [t2] from [t1], returning a normalized result. *)
   val sub : t -> t -> t
 
   (** [add_sec s t] adds [s] seconds to [t]. *)
   val add_sec : int64 -> t -> t
 
-  (** [add_sec ns t] adds [ns] nanoseconds to [t]. *)
+  (** [add_nsec ns t] adds [ns] nanoseconds to [t]. *)
   val add_nsec : int64 -> t -> t
 
   (** [sub_sec s t] subtracts [s] seconds from [t]. *)
   val sub_sec : int64 -> t -> t
 
-  (** [sub_sec ns t] subtracts [ns] nanoseconds from [t]. *)
+  (** [sub_nsec ns t] subtracts [ns] nanoseconds from [t]. *)
   val sub_nsec : int64 -> t -> t
 
-  (** [to_string t] creates a string of the form "sec.nsec". *)
+  (** [to_string t] returns a string in the form ["sec.nsec"]. *)
   val to_string : t -> string
 
-  (** [compare t1 t2] compares the two time values [t1] and [t2]. It returns [0]
-      if [t1] is equal to [2], a negative integer if [t1] is less than [t2], and
-      a positive integer if [t1] is greater than [t2]. *)
+  (** [compare t1 t2] compares two time values. Returns [0] if equal,
+      negative if [t1 < t2], positive if [t1 > t2]. *)
   val compare : t -> t -> int
 end
 
+(** Interval timer specification using timespec. *)
 module Itimerspec : sig
-  type t = { it_interval : Timespec.t; it_value : Timespec.t }
+  type t = {
+    it_interval : Timespec.t;  (** Timer interval *)
+    it_value : Timespec.t;     (** Initial expiration *)
+  }
 end
 
+(** POSIX timeval: time with microsecond precision. *)
 module Timeval : sig
-  (** POSIX timeval time value with seconds and microseconds. A normalized value
-      of type [t] must have a value of [tv_usec] between [0] and [1000000]
-      (exclusive). *)
+  (** Time value with seconds and microseconds. A normalized value has [tv_usec]
+      between [0] and [999999] (inclusive). *)
   type t = private { tv_sec : int64; tv_usec : int64 }
 
-  (** [create sec usec] creates a new normalized timespec with [sec] seconds and
-      [usec] microseconds. *)
+  (** [create sec usec] creates a new normalized timeval. *)
   val create : int64 -> int64 -> t
 
-  (** [add t1 t2] adds the two timespec values, returning a normalized value *)
+  (** [add t1 t2] adds two timeval values, returning a normalized result. *)
   val add : t -> t -> t
 
-  (** [sub t1 t2] subtracts [t2] from [t1], creating a new normalized time
-      value. *)
+  (** [sub t1 t2] subtracts [t2] from [t1], returning a normalized result. *)
   val sub : t -> t -> t
 
   (** [add_sec s t] adds [s] seconds to [t]. *)
   val add_sec : int64 -> t -> t
 
-  (** [add_sec us t] adds [us] microseconds to [t]. *)
+  (** [add_usec us t] adds [us] microseconds to [t]. *)
   val add_usec : int64 -> t -> t
 
   (** [sub_sec s t] subtracts [s] seconds from [t]. *)
   val sub_sec : int64 -> t -> t
 
-  (** [sub_sec us t] subtracts [us] microseconds from [t]. *)
+  (** [sub_usec us t] subtracts [us] microseconds from [t]. *)
   val sub_usec : int64 -> t -> t
 
-  (** [to_string t] creates a string of the form "sec.usec". *)
+  (** [to_string t] returns a string in the form ["sec.usec"]. *)
   val to_string : t -> string
 
-  (** [compare t1 t2] compares the two time values [t1] and [t2]. It returns [0]
-      if [t1] is equal to [2], a negative integer if [t1] is less than [t2], and
-      a positive integer if [t1] is greater than [t2]. *)
+  (** [compare t1 t2] compares two time values. Returns [0] if equal,
+      negative if [t1 < t2], positive if [t1 > t2]. *)
   val compare : t -> t -> int
 end
 
+(** POSIX broken-down time structure. *)
 module Tm : sig
-  (** POSIX tm value with seconds [0,61], minutes [0,59], hours [0,23], day of
-      month [0,31], month of year [0,11], years since 1900, day of week [0,6]
-      (Sunday = 0), day of year [0,365] and daylight saving flag. The daylight
-      saving flag is positive if daylight saving is in effect and 0 if not. In
-      case this information is not available, it has a negative value. *)
+  (** Broken-down time with fields for seconds [0-60], minutes [0-59],
+      hours [0-23], day of month [1-31], month [0-11], years since 1900,
+      day of week [0-6] (Sunday = 0), day of year [0-365], and daylight
+      saving flag (positive if DST, 0 if not, negative if unknown). *)
   type t = private {
     tm_sec : int;
     tm_min : int;
@@ -102,45 +105,118 @@ module Tm : sig
     tm_isdst : int;
   }
 
-  (** [create tm_sec tm_min tm_hour tm_mday rm_mon tm_year tm_wday tm_yday
-       tm_isdst] creates a new time value if the all arguments suffice the
-      aforementioned predicates. Otherwise [create] will return [None]. *)
+  (** [create sec min hour mday mon year wday yday isdst] creates a new
+      broken-down time value. Returns [None] if arguments are invalid. *)
   val create :
     int -> int -> int -> int -> int -> int -> int -> int -> int -> t option
 
-  (** [compare t1 t2] compares the two time values [t1] and [t2]. It returns [0]
-      if [t1] is equal to [2], a negative integer if [t1] is less than [t2], and
-      a positive integer if [t1] is greater than [t2]. [compare] considers
-      tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec in the given order. *)
+  (** [compare t1 t2] compares two time values considering year, month, day,
+      hour, minute, second in that order. *)
   val compare : t -> t -> int
 end
 
+(** Interval timer value using timeval. *)
 module Itimerval : sig
-  type t = { it_interval : Timeval.t; it_value : Timeval.t }
+  type t = {
+    it_interval : Timeval.t;  (** Timer interval *)
+    it_value : Timeval.t;     (** Current value *)
+  }
 end
 
-type clock = [ `Realtime | `Monotonic | `Process_cputime | `Thread_cputime ]
+(** {1 Clock Types} *)
 
+(** Clock identifiers for {!clock_gettime} and related functions.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_getres.html} clock_getres(3)}. *)
+type clock =
+  [ `Realtime        (** System-wide real-time clock *)
+  | `Monotonic       (** Monotonic clock that cannot be set *)
+  | `Process_cputime (** CPU time consumed by the process *)
+  | `Thread_cputime  (** CPU time consumed by the thread *)
+  ]
+
+(** {1 Clock Functions} *)
+
+(** Convert broken-down time to a string.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/asctime.html} asctime(3)}. *)
 val asctime : Tm.t -> string
+
+(** Get clock resolution.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_getres.html} clock_getres(3)}. *)
 val clock_getres : clock -> Timespec.t
+
+(** Get current time from a clock.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_gettime.html} clock_gettime(3)}. *)
 val clock_gettime : clock -> Timespec.t
+
+(** Set a clock's time (requires appropriate privileges).
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_settime.html} clock_settime(3)}. *)
 val clock_settime : clock -> Timespec.t -> unit
+
+(** Convert Unix timestamp to a date/time string.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/ctime.html} ctime(3)}. *)
 val ctime : int64 -> string
+
+(** Convert Unix timestamp to broken-down UTC time.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/gmtime.html} gmtime(3)}. *)
 val gmtime : int64 -> Tm.t
+
+(** Convert Unix timestamp to broken-down local time.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/localtime.html} localtime(3)}. *)
 val localtime : int64 -> Tm.t
+
+(** Convert broken-down time to Unix timestamp.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/mktime.html} mktime(3)}. *)
 val mktime : Tm.t -> int64
+
+(** {1 Sleep Functions} *)
+
+(** High-resolution sleep.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/nanosleep.html} nanosleep(3)}. *)
 val nanosleep : Timespec.t -> unit
+
+(** High-resolution sleep with clock selection.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_nanosleep.html} clock_nanosleep(3)}.
+
+    @param absolute If [true], sleep until the specified absolute time.
+                    If [false], sleep for the specified duration.
+    @param clock The clock to use for timing. *)
 val clock_nanosleep : absolute:bool -> clock:clock -> Timespec.t -> unit
 
-type itimer = [ `Real | `Virtual | `Prof ]
+(** {1 Interval Timers} *)
 
+(** Interval timer types for {!getitimer} and {!setitimer}.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/getitimer.html} getitimer(3)}. *)
+type itimer =
+  [ `Real    (** ITIMER_REAL: decrements in real time, delivers SIGALRM *)
+  | `Virtual (** ITIMER_VIRTUAL: decrements in process virtual time, delivers SIGVTALRM *)
+  | `Prof    (** ITIMER_PROF: decrements in process time, delivers SIGPROF *)
+  ]
+
+(** Get the value of an interval timer.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/getitimer.html} getitimer(3)}. *)
 val getitimer : itimer -> Itimerval.t
+
+(** Set an interval timer and return its previous value.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/setitimer.html} setitimer(3)}. *)
 val setitimer : itimer -> Itimerval.t -> Itimerval.t
+
+(** {1 Time of Day} *)
+
+(** Get the current time of day.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/gettimeofday.html} gettimeofday(3)}. *)
 val gettimeofday : unit -> Timeval.t
 
-(* [select] behaviour should be exactly the same as the C equivalent.
- * Passing [None] for [timeval] is equivalent to passing a [NULL] pointer
- * in a C call. *)
+(** {1 I/O Multiplexing} *)
+
+(** Synchronous I/O multiplexing.
+
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/select.html} select(2)}.
+
+    @param readfds File descriptors to check for readability.
+    @param writefds File descriptors to check for writability.
+    @param exceptfds File descriptors to check for exceptions.
+    @param timeout Maximum time to wait, or [None] to block indefinitely.
+    @return Triple of ready file descriptors [(readable, writable, exceptions)]. *)
 val select :
   Unix.file_descr list ->
   Unix.file_descr list ->
@@ -148,4 +224,11 @@ val select :
   Timeval.t option ->
   Unix.file_descr list * Unix.file_descr list * Unix.file_descr list
 
+(** {1 File Times} *)
+
+(** Set file access and modification times.
+    See {{:https://pubs.opengroup.org/onlinepubs/9699919799/functions/utimes.html} utimes(3)}.
+
+    @param path Path to the file.
+    @param times The access and modification time to set. *)
 val utimes : string -> Timeval.t -> unit
